cf089411b3ae0c57035dbb35a012701324ba514a
Fakechippies <cherryprofessorpraveen@gmail.com>
2024-08-25 14:06:22 +0530

    URL Encoding & Decoding done

--- Encoding/encoding.go
+++ Encoding/encoding.go
@@ -4,6 +4,7 @@ import (
 	b32 "encoding/base32"
 	b64 "encoding/base64"
 	"fmt"
+	"net/url"
 
 	"github.com/btcsuite/btcutil/base58"
 )
@@ -19,6 +20,10 @@ func Parser(encMethod string, decode bool, encode bool, input string) string {
 
 	if encMethod == "base58" {
 		return base58Conv(decode, encode, input)
+	}
+
+	if encMethod == "URL" {
+		return URLConv(decode, encode, input)
 	}
 	return "Unsupported encoding method"
 
@@ -65,6 +70,22 @@ func base58Conv(decode bool, encode bool, input string) string {
 	} else if decode {
 		data := base58.Decode(input)
 		result = string(data)
+	}
+	return result
+}
+
+func URLConv(decode bool, encode bool, input string) string {
+	var result string
+
+	if encode {
+		result = url.QueryEscape(input)
+	} else if decode {
+		data, err := url.QueryUnescape(input)
+		if err != nil {
+			fmt.Println("Error Decoding : %s", err)
+			return ""
+		}
+		result = data
 	}
 	return result
 }
