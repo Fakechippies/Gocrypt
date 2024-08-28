acff20330ce35caa21aa717ffb3970c0ea56ed0a
Fakechippies <cherryprofessorpraveen@gmail.com>
2024-08-29 01:05:16 +0530

    Added a Verbose Mode

--- main.go
+++ main.go
@@ -10,6 +10,9 @@ import (
 )
 
 func main() {
+	// Verbose flag
+	vflag := flag.Bool("v", false, "Enable the verbose mode")
+
 	// Encoding flags
 	encFlag := flag.String("enc", "", "Enter the Encoding method")
 	decryptFlag := flag.Bool("d", false, "Decode the input")
@@ -31,18 +34,34 @@ func main() {
 		result := encoding.Parser(*encFlag, *decryptFlag, *encryptFlag, flag.Args()[0])
 		if result != "" {
 			if *decryptFlag {
-				fmt.Printf("Decoded from %s : %s\n", *encFlag, result)
+				if *vflag {
+					fmt.Printf("Decoded from %s : %s\n", *encFlag, result)
+				} else {
+					fmt.Printf("%s\n", result)
+				}
 			} else if *encryptFlag {
-				fmt.Printf("Encoded to %s : %s\n", *encFlag, result)
+				if *vflag {
+					fmt.Printf("Encoded to %s : %s\n", *encFlag, result)
+				} else {
+					fmt.Printf("%s\n", result)
+				}
 			}
 		}
 	} else if *hashFlag != "" {
 		result := hashing.Parser(*hashFlag, flag.Args()[0])
-		fmt.Printf("'%s' hash of string '%s' : %s\n", *hashFlag, flag.Args()[0], result)
+		if *vflag {
+			fmt.Printf("'%s' hash of string '%s' : %s\n", *hashFlag, flag.Args()[0], result)
+		} else {
+			fmt.Printf("%s\n", result)
+		}
 	} else if *baseFlag {
 		result := baseconversion.Parser(*fromFlag, *toFlag, flag.Args()[0])
 		if result != "" {
-			fmt.Printf("'%s' conversion from %s to %s : %s \n", flag.Args()[0], *fromFlag, *toFlag, result)
+			if *vflag {
+				fmt.Printf("'%s' conversion from %s to %s : %s \n", flag.Args()[0], *fromFlag, *toFlag, result)
+			} else {
+				fmt.Printf("%s\n", result)
+			}
 		} else {
 			fmt.Println("Error in conversion!!!")
 		}
