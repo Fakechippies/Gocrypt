672b3eff150a6e83c63c0a045420d088d6ed7816
sohamt <sohamtanwar5@gmail.com>
2024-08-29 01:41:43 +0530

    Added rot encryption and decryption, Still working on brute forcing

---
+++ ROT-Conversion/rot-conversion.go
@@ -0,0 +1,44 @@
+package ROT
+
+func Parser(rot_type int, encrypt bool, decrypt bool, input string) string {
+	if encrypt {
+		return rot(input, rot_type)
+	} else if decrypt {
+		if rot_type%13 == 0 {
+			return rot(input, rot_type)
+		} else if rot_type <= 25 && rot_type >= 1 {
+			return rot(input, 26-rot_type)
+		} else {
+			return rot(input, (-1)*(rot_type-26))
+		}
+	}
+	return "Error in rot type provided or input"
+}
+
+func rot(input string, shift int) string {
+	if shift > 26 {
+		shift = shift - 26
+	}
+
+	letters := [26]string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"}
+
+	var lengthText = len([]rune(input))
+
+	var result string
+
+	for i := 0; i < lengthText; i++ {
+		for a := 0; a < 26; a++ {
+			if string(input[i]) == letters[a] {
+				if a+shift > 25 {
+					index := (a + shift) - 26
+					result = result + letters[index]
+				} else {
+					result = result + letters[a+shift]
+				}
+			}
+		}
+	}
+
+	return result
+
+}
--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"GoCrypt/Base-Conversion"
 	"GoCrypt/Encoding"
 	"GoCrypt/Hashing"
+	"GoCrypt/ROT-Conversion"
 )
 
 func main() {
@@ -24,6 +25,9 @@ func main() {
 	toFlag := flag.String("t", "", "new encoding method")
 
 	// Add more flags here if needed
+	rotFlag := flag.Int("rot", 0, "Enter ROT type")
+	encrFlag := flag.Bool("encr", false, "encrypt the input")
+	decrFlag := flag.Bool("decr", false, "decrypt the input")
 
 	flag.Parse()
 
@@ -35,6 +39,13 @@ func main() {
 			} else if *encryptFlag {
 				fmt.Printf("Encoded to %s : %s\n", *encFlag, result)
 			}
+		}
+	} else if *rotFlag != 0 {
+		result := ROT.Parser(*rotFlag, *encrFlag, *decrFlag, flag.Args()[0])
+		if *encrFlag {
+			fmt.Printf("Encrypted in ROT%d : %s", *rotFlag, result)
+		} else if *decrFlag {
+			fmt.Printf("Decrypted ROT%d : %s", *rotFlag, result)
 		}
 	} else if *hashFlag != "" {
 		result := hashing.Parser(*hashFlag, flag.Args()[0])
