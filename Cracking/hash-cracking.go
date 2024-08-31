d5b8888f3fb6e017ccb47da69268907b14ab2135
Fakechippies <cherryprofessorpraveen@gmail.com>
2024-08-31 13:41:34 +0530

    Added sha1 hash cracking

--- Cracking/hash-cracking.go
+++ Cracking/hash-cracking.go
@@ -42,3 +42,29 @@ func md5Crack(hashString string, wordlistPath string) string {
 	fmt.Println("Cannot crack password")
 	return ""
 }
+
+func sha1Crack(hashString string, wordlistPath string) string {
+	file, err := os.Open(wordlistPath)
+	if err != nil {
+		fmt.Println("Error opening wordlist : ", err)
+		return ""
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		word := scanner.Text()
+		hashedWord := hashing.Parser("sha1", word)
+		if hashedWord == hashString {
+			return word
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error reading wordlist : ", err)
+		return ""
+	}
+
+	fmt.Println("Cannot crack password")
+	return ""
+}
