3def2cd8faf7c4c974dbdb9cecdae3ea1ba42f6c
Fakechippies <cherryprofessorpraveen@gmail.com>
2024-08-31 13:58:48 +0530

    hashing universal password cracking

--- Cracking/hash-cracking.go
+++ Cracking/hash-cracking.go
@@ -9,19 +9,10 @@ import (
 )
 
 func Parser(hashMethod string, wordlistPath string, hashString string) string {
-	if hashMethod == "md5" {
-		return md5Crack(hashString, wordlistPath)
-	} else if hashMethod == "sha1" {
-		return sha1Crack(hashString, wordlistPath)
-	} else if hashMethod == "sha224" {
-		return sha224Crack(hashString, wordlistPath)
-	} else if hashMethod == "sha256" {
-		return sha256Crack(hashString, wordlistPath)
-	}
-	return "Password Cracking Method not found"
+	return Crack(hashString, wordlistPath, hashMethod)
 }
 
-func md5Crack(hashString string, wordlistPath string) string {
+func Crack(hashString string, wordlistPath string, hashMethod string) string {
 	file, err := os.Open(wordlistPath)
 	if err != nil {
 		fmt.Println("Error opening wordlist : ", err)
@@ -32,7 +23,7 @@ func md5Crack(hashString string, wordlistPath string) string {
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		word := scanner.Text()
-		hashedWord := hashing.Parser("md5", word)
+		hashedWord := hashing.Parser(hashMethod, word)
 		if hashedWord == hashString {
 			return word
 		}
@@ -40,84 +31,6 @@ func md5Crack(hashString string, wordlistPath string) string {
 
 	if err := scanner.Err(); err != nil {
 		fmt.Println("Error reading wordlist:", err)
-		return ""
-	}
-
-	fmt.Println("Cannot crack password")
-	return ""
-}
-
-func sha1Crack(hashString string, wordlistPath string) string {
-	file, err := os.Open(wordlistPath)
-	if err != nil {
-		fmt.Println("Error opening wordlist : ", err)
-		return ""
-	}
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		word := scanner.Text()
-		hashedWord := hashing.Parser("sha1", word)
-		if hashedWord == hashString {
-			return word
-		}
-	}
-
-	if err := scanner.Err(); err != nil {
-		fmt.Println("Error reading wordlist : ", err)
-		return ""
-	}
-
-	fmt.Println("Cannot crack password")
-	return ""
-}
-
-func sha224Crack(hashString string, wordlistPath string) string {
-	file, err := os.Open(wordlistPath)
-	if err != nil {
-		fmt.Println("Error opening wordlist : ", err)
-		return ""
-	}
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		word := scanner.Text()
-		hashedWord := hashing.Parser("sha224", word)
-		if hashedWord == hashString {
-			return word
-		}
-	}
-
-	if err := scanner.Err(); err != nil {
-		fmt.Println("Error reading wordlist : ", err)
-		return ""
-	}
-
-	fmt.Println("Cannot crack password")
-	return ""
-}
-
-func sha256Crack(hashString string, wordlistPath string) string {
-	file, err := os.Open(wordlistPath)
-	if err != nil {
-		fmt.Println("Error opening wordlist : ", err)
-		return ""
-	}
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		word := scanner.Text()
-		hashedWord := hashing.Parser("sha256", word)
-		if hashedWord == hashString {
-			return word
-		}
-	}
-
-	if err := scanner.Err(); err != nil {
-		fmt.Println("Error reading wordlist : ", err)
 		return ""
 	}
 
